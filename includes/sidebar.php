5d65ccddfeba2dc830e8d95ad85ceaeff8907cda
iaex-2git <imarev_ex@yahoo.com>
2025-12-23 12:02:25 +0600

    new project

---
+++ includes/sidebar.php
@@ -0,0 +1,112 @@
+<aside class="sidebar">
+								<form action="page-search-results.html" method="get">
+									<div class="input-group mb-3 pb-1">
+										<input class="form-control text-1" placeholder="Search..." name="s" id="s" type="text">
+										<button type="submit" class="btn btn-dark text-1 p-2"><i class="fas fa-search m-2"></i></button>
+									</div>
+								</form>
+								<h5 class="font-weight-semi-bold pt-3">Categories</h5>
+								<ul class="nav nav-list flex-column">
+									<li class="nav-item"><a class="nav-link" href="#">Arts & Crafts</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Automotive</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Baby</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Books</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Eletronics</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Women's Fashion</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Men's Fashion</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Health & Household</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Home & Kitchen</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Military Accessories</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Movies & Television</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Sports & Outdoors</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Tools & Home Improvement</a></li>
+									<li class="nav-item"><a class="nav-link" href="#">Toys & Games</a></li>
+								</ul>
+								<h5 class="font-weight-semi-bold pt-5">Tags</h5>
+								<div class="mb-3 pb-1">
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">Nike</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">Travel</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">Sport</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">TV</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">Books</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">Tech</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">Adidas</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">Promo</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">Reading</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">Social</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">Books</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">Tech</span></a>
+									<a href="#"><span class="badge badge-dark badge-sm rounded-pill text-uppercase px-2 py-1 me-1">New</span></a>
+								</div>
+								<div class="row mb-5">
+									<div class="col">
+										<h5 class="font-weight-semi-bold pt-5">Top Rated Products</h5>
+										<div class="product row row-gutter-sm align-items-center mb-4">
+											<div class="col-5 col-lg-5">
+												<div class="product-thumb-info border-0">
+													<a href="shop-product.html">
+														<div class="product-thumb-info-image">
+															<img alt="" class="img-fluid" src="img/products/product-grey-6.jpg">
+														</div>
+													</a>
+												</div>
+											</div>
+											<div class="col-7 col-lg-7 ms-md-0 ms-lg-0 ps-lg-1 pt-1">
+												<a href="#" class="d-block text-uppercase text-decoration-none text-color-default text-color-hover-primary line-height-1 text-0 mb-2">hat</a>
+												<h3 class="text-3-5 font-weight-medium font-alternative text-transform-none line-height-3 mb-0"><a href="shop-product-sidebar-right.html" class="text-color-dark text-color-hover-primary text-decoration-none">Blue Hat</a></h3>
+												<div title="Rated 5 out of 5">
+													<input type="text" class="d-none" value="5" title="" data-plugin-star-rating data-plugin-options="{'displayOnly': true, 'color': 'dark', 'size':'xs'}">
+												</div>
+												<p class="price text-4 mb-0">
+													<span class="sale text-color-dark font-weight-semi-bold">$299,00</span>
+													<span class="amount">$289,00</span>
+												</p>
+											</div>
+										</div>
+										<div class="product row row-gutter-sm align-items-center mb-4">
+											<div class="col-5 col-lg-5">
+												<div class="product-thumb-info border-0">
+													<a href="shop-product.html">
+														<div class="product-thumb-info-image">
+															<img alt="" class="img-fluid" src="img/products/product-grey-8.jpg">
+														</div>
+													</a>
+												</div>
+											</div>
+											<div class="col-7 col-lg-7 ms-md-0 ms-lg-0 ps-lg-1 pt-1">
+												<a href="#" class="d-block text-uppercase text-decoration-none text-color-default text-color-hover-primary line-height-1 text-0 mb-2">accessories</a>
+												<h3 class="text-3-5 font-weight-medium font-alternative text-transform-none line-height-3 mb-0"><a href="shop-product-sidebar-right.html" class="text-color-dark text-color-hover-primary text-decoration-none">Adventurer Bag</a></h3>
+												<div title="Rated 5 out of 5">
+													<input type="text" class="d-none" value="5" title="" data-plugin-star-rating data-plugin-options="{'displayOnly': true, 'color': 'dark', 'size':'xs'}">
+												</div>
+												<p class="price text-4 mb-0">
+													<span class="sale text-color-dark font-weight-semi-bold">$99,00</span>
+													<span class="amount">$79,00</span>
+												</p>
+											</div>
+										</div>
+										<div class="product row row-gutter-sm align-items-center mb-4">
+											<div class="col-5 col-lg-5">
+												<div class="product-thumb-info border-0">
+													<a href="shop-product.html">
+														<div class="product-thumb-info-image">
+															<img alt="" class="img-fluid" src="img/products/product-grey-9.jpg">
+														</div>
+													</a>
+												</div>
+											</div>
+											<div class="col-7 col-lg-7 ms-md-0 ms-lg-0 ps-lg-1 pt-1">
+												<a href="#" class="d-block text-uppercase text-decoration-none text-color-default text-color-hover-primary line-height-1 text-0 mb-2">sports</a>
+												<h3 class="text-3-5 font-weight-medium font-alternative text-transform-none line-height-3 mb-0"><a href="shop-product-sidebar-right.html" class="text-color-dark text-color-hover-primary text-decoration-none">Baseball Ball</a></h3>
+												<div title="Rated 5 out of 5">
+													<input type="text" class="d-none" value="5" title="" data-plugin-star-rating data-plugin-options="{'displayOnly': true, 'color': 'dark', 'size':'xs'}">
+												</div>
+												<p class="price text-4 mb-0">
+													<span class="sale text-color-dark font-weight-semi-bold">$399,00</span>
+													<span class="amount">$299,00</span>
+												</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</aside>
